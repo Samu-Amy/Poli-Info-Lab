7e2bbb6af3bf7b7e21ff9e7c0c80cd64dca61d32
Samu-Amy <amisanosamuele@gmail.com>
2022-04-01 17:44:50 +0200

    Aggiornamento Lab 5 es 3

--- Lab_5/Es3.py
+++ Lab_5/Es3.py
@@ -127,7 +127,7 @@ def remove():
 def endRound():
     window = Toplevel()
     window.title("Game over")
-    window.geometry("200x200+550+300")
+    window.geometry("200x200+550+300")l
     window.focus()
     window.columnconfigure(0, weight=1)
 
