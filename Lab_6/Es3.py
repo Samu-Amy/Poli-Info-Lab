f065306d01d54be6dce909f0b4cbf4b79df0c2a0
Samu Amy <65444098+Samu-Amy@users.noreply.github.com>
2022-04-08 11:21:11 +0200

    Aggiornamento Lab 6 es 3, 4

---
+++ Lab_6/Es3.py
@@ -0,0 +1,65 @@
+class Player:
+
+    def __init__(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.team = None
+
+    def get_values(self):
+        return [self.name, self.surname, self.age]
+
+    def set_team(self, team):
+        self.team = team
+
+    def get_team(self):
+        return self.team
+
+class Team:
+
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+        self.players = []
+
+    def get_info(self):
+        return [self.name, self.city]
+
+    def add_player(self, player):
+        self.players.append(player)
+
+    def get_players(self):
+        return self.players
+
+
+# Test
+def main():
+
+    player1 = Player("Gianni", "Rossi", 21)
+    player2 = Player("Bob", "Green", 24)
+
+    team1 = Team("Red", "Torino")
+
+
+    player1.set_team(team1)
+    player2.set_team(team1)
+
+    team1.add_player(player1)
+    team1.add_player(player2)
+
+
+    info = player1.get_values()
+    print(f"\nDettagli giocatore 1: {info[0]} {info[1]}, {info[2]} anni")
+
+    info = player2.get_values()
+    print(f"Dettagli giocatore 2: {info[0]} {info[1]}, {info[2]} anni")
+
+    print(f"Fanno parte della squadra: {player1.get_team().get_info()[0]}")
+
+    teamInfo = team1.get_info()
+    print(f"Dettagli squadra: nome {teamInfo[0]}, città {teamInfo[1]}")
+
+    teamPlayers = team1.get_players()
+    print(f"Di cui fanno parte: {teamPlayers[0].get_values()[0]} {teamPlayers[0].get_values()[1]} e {teamPlayers[1].get_values()[0]} {teamPlayers[1].get_values()[1]}")
+
+main()
---
+++ Lab_6/Es4.py
@@ -0,0 +1,119 @@
+class Matrix:
+
+    def __init__(self, matrice):
+        self.matrice = [["",""],["",""]]
+        for i in range(len(matrice)):
+            for j in range(len(matrice[i])):
+                self.matrice[i][j] = matrice[i][j]
+
+    # def get_rows(self):
+    #     for i in range(len(self.matrice)):
+    #         print(self.matrice[i])
+    #
+    # def get_columns(self):
+    #     for i in range(len(self.matrice)):
+    #         for j in range(len(self.matrice[i])):
+    #             print(self.matrice[j][i])
+    #         print()
+
+    def __add__(self, other):
+        tab = [["",""], ["",""]]
+        for i in range(len(self.matrice)):
+            for j in range(len(self.matrice[i])):
+                tab[i][j] = self.matrice[i][j] + other.matrice[i][j]
+
+        return Matrix(tab)
+
+    def __sub__(self, other):
+        tab = [["",""], ["",""]]
+        for i in range(len(self.matrice)):
+            for j in range(len(self.matrice[i])):
+                tab[i][j] = self.matrice[i][j] - other.matrice[i][j]
+
+        return Matrix(tab)
+
+    def __mul__(self, other):
+        tab = [["",""], ["",""]]
+        for i in range(len(self.matrice)):
+            for j in range(len(self.matrice[i])):
+                tab[i][j] = self.matrice[i][j] * other.matrice[i][j]
+
+        return Matrix(tab)
+
+    def __eq__(self, other):
+        uguali = True
+        for i in range(len(self.matrice)):
+            for j in range(len(self.matrice[i])):
+                if self.matrice[i][j] != other.matrice[i][j]:
+                    uguali = False
+                    break
+        return uguali
+
+    def __str__(self):
+        string = ""
+        for i in range(len(self.matrice)):
+            for j in range(len(self.matrice[i])):
+                string += str(self.matrice[i][j]) + " "
+            if i < len(self.matrice) - 1: #Se non è all'ultima riga
+                string += "/ "
+
+        return string
+
+    def stampa(self):
+        for i in range(len(self.matrice)):
+            for j in range(len(self.matrice[i])):
+                print(self.matrice[i][j], end=" ")
+            print()
+
+
+def main():
+
+    tabella1 = [[2,3], [8,5]]
+    tabella2 = [[4,6], [9,2]]
+
+    matrice1 = Matrix(tabella1)
+    matrice2 = Matrix(tabella2)
+    matrice4 = Matrix(tabella1)
+    matrice5 = matrice1
+
+    # matrice1.get_rows()
+    # matrice1.get_columns()
+
+    matrice1.stampa()
+    print()
+    matrice2.stampa()
+
+    print("\nSomma:")
+    matrice3 = matrice1 + matrice2
+    matrice3.stampa()
+
+    print("\nSottrazione:")
+    matrice3 = matrice1 - matrice2
+    matrice3.stampa()
+
+    print("\nMoltiplicazione:")
+    matrice3 = matrice1 * matrice2
+    matrice3.stampa()
+
+    print("\nUguaglianza tra 1 e 2:")
+    if matrice1 == matrice2:
+        print("Sono uguali")
+    else:
+        print("Sono diverse")
+
+    print("\nUguaglianza tra 1 e 4:")
+    if matrice1 == matrice4:
+        print("Sono uguali")
+    else:
+        print("Sono diverse")
+
+    print("\nUguaglianza tra 1 e 5:")
+    if matrice1 == matrice5:
+        print("Sono uguali")
+    else:
+        print("Sono diverse")
+
+    print(f"Stringa matrice 1: {str(matrice1)}")
+
+
+main()
