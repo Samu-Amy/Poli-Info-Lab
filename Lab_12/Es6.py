316c4cfd6a5004f8f775c980e5c6a778f06a7392
Samu-Amy <amisanosamuele@gmail.com>
2022-06-04 19:00:31 +0200

    Aggiornamento Lab 12

--- Lab_12/Es6.py
+++ Lab_12/Es6.py
@@ -14,12 +14,21 @@ class BinarySearchTree:
         return self._nodes
 
     def get_min(self):
-        min = self._head.min(self._head)
-        return min
+        minimum = self._head.min(self._head)
+        return minimum
 
     # TODO: aggiungi metodo di stampa (ordinata ma senza algoritmi di sorting)
     def __repr__(self):
-        pass
+        datas = []
+        minimum = self.get_min()
+        datas.append(minimum.get())
+        datas.append(minimum.get_previous().get())
+        minimum.get_greater()
+        string = ""
+        for value in datas:
+            string += str(value)
+            print(value)
+        return string
 
 
 class Node:
@@ -28,6 +37,10 @@ class Node:
         self._data = data
         self._less = None
         self._greater = None
+        self._previous = None
+
+    def set_previous(self, node):
+        self._previous = node
 
     def get(self):
         return self._data
@@ -35,17 +48,27 @@ class Node:
     def get_less(self):
         return self._less
 
+    def get_greater(self):
+        return self._greater
+
+    def get_previous(self):
+        return self._previous
+
     def get_all(self):
         string = ""
         string += str(self._data) + ", "
         try:
             string += str(self._less.get()) + ", "
         except AttributeError:
-            string += str(self._less) + " "
+            string += "N, "
         try:
-            string += str(self._greater.get())
+            string += str(self._greater.get()) + ", "
         except AttributeError:
-            string += str(self._greater)
+            string += "N, "
+        try:
+            string += str(self._previous.get())
+        except AttributeError:
+            string += str(self._previous)
         return string
 
     def __le__(self, other):
@@ -55,6 +78,7 @@ class Node:
         return self._data > other.get()
 
     def add_node(self, other):
+        other.set_previous(self)
         if other <= self:
             if self._less is None:
                 self._less = other
@@ -72,13 +96,23 @@ class Node:
         else:
             return node
 
+    def to_max(self, node, values):
+        if node.get_greater() is not None:
+            return self.to_max(node.get_greater(), values)
+        else:
+            return node
 
-node1 = Node(5)
-node2 = Node(2)
-node3 = Node(10)
+
+node1 = Node(10)
+node2 = Node(5)
+node3 = Node(2)
 node4 = Node(6)
-node5 = Node(12)
+node5 = Node(15)
 node6 = Node(1)
+node7 = Node(12)
+node8 = Node(18)
+node9 = Node(16)
+node10 = Node(8)
 
 tree = BinarySearchTree(node1)
 tree.add(node2)
@@ -86,6 +120,10 @@ tree.add(node3)
 tree.add(node4)
 tree.add(node5)
 tree.add(node6)
+tree.add(node7)
+tree.add(node8)
+tree.add(node9)
+tree.add(node10)
 
 
 list = tree.get_nodes()
@@ -95,3 +133,5 @@ for node in list:
 
 
 print(f"\nMinimo: {tree.get_min().get()}")
+
+print(tree)
