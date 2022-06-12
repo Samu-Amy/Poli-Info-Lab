e5a827f66d14c5af4d2c4038700f6f6dd28b856f
Samu-Amy <amisanosamuele@gmail.com>
2022-06-12 12:25:28 +0200

    Aggiornamento Lab 12

--- Lab_12/Es3.py
+++ Lab_12/Es3.py
@@ -1,73 +1,35 @@
 
 class Image:
 
-    def __init__(self, img: list) -> None:
-        self._img = img
+    def __init__(self, image):
+        self._image = image
 
-    def get_img(self):
-        return self._img
+    def paint_bucket(self, color, pos, initial=None):
+        i = pos[0]
+        j = pos[1]
+        initial_color = self._image[i][j]
+        self._image[i][j] = color
+        for i in range(max(0, pos[0] - 1), min(pos[0] + 1, len(self._image))):
+            for j in range(max(0, pos[1] - 1), min(pos[1] + 1, len(self._image[i]))):
+                if self._image[i][j] == initial_color:
+                    pos = i, j
+                    self.paint_bucket(color, pos, initial_color)
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         string = ""
-        for i in range(len(self._img)):
-            for j in range(len(self._img[i])):
-                string += str(self._img[i][j]) + "  "
+        for i in range(len(self._image)):
+            for j in range(len(self._image[i])):
+                string += str(self._image[i][j]) + "  "
             string += "\n"
         return string
 
 
-def stampa(l):
-    string = ""
-    for i in range(len(l)):
-        for j in range(len(l[i])):
-            string += str(l[i][j]) + "  "
-        string += "\n"
-    print(string)
-
-
-def paint_bucket(img, color, pos, all_near):
-    x, y = pos[0], pos[1]
-    old_color = img[x][y]
-    near = []
-
-    for i in range(-1, 2):
-        for j in range(-1, 2):
-            try:
-                if img[x+i][y+j] == old_color and (x+i, y+j) != (x, y) and (x+i, y+j) not in all_near:  # Se è del vecchio colore e non è il punto su cui è stato eseguito
-                    all_near.append((x+i, y+j))
-                    near.append((x+i, y+j))
-            except IndexError:
-                pass
-    print(near)
-    print(all_near)
-    print()
-
-    if len(near) != 0:
-        img[x][y] = color
-        # paint_bucket(img, color, near[0], all_near)
-        # try:
-        #     paint_bucket(img, color, near[1], all_near)
-        # except IndexError:
-        #     pass
-        for pos in all_near:
-            paint_bucket(img, color, pos, all_near)
-            # stampa(img)
-
-
-list = [[0, 5, 1, 2, 3, 2], [2, 1, 6, 2, 2, 2], [4, 8, 2, 9, 5, 2], [2, 2, 8, 2, 1, 2], [3, 7, 9, 2, 2, 2], [6, 5, 9, 8, 2, 4]]
-color = 5
+img = [[0, 5, 1, 2, 3, 2], [2, 1, 6, 2, 2, 2], [4, 8, 2, 9, 5, 2], [2, 2, 8, 2, 1, 2], [3, 7, 9, 2, 2, 2], [6, 5, 9, 8, 2, 4]]
+image = Image(img)
+col = 0
 position = (4, 5)
 
 
-stampa(list)
-
-paint_bucket(list, color, position, [])
-
-stampa(list)
-
-
-# image = Image(l)
-#
-# print(image)
-
-
+print(image)
+image.paint_bucket(col, position)
+print(image)
